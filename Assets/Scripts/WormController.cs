4013ded1872ea03e4c51029301858f8d6be4313e
max <max.stienitzka@gmail.com>
2020-05-22 15:07:16 +0200

    WormControllerScript added and functional

---
+++ Assets/Scripts/WormController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode jumpKey;
+    public KeyCode fowardKey;
+    public Rigidbody z;
+
+    public Vector3 x;
+    public Vector3 y;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Debug.Log("Jump wurde gedrückt");
+            z.AddForce(x);
+        }
+
+        if (Input.GetKeyDown(fowardKey))
+        {
+            Debug.Log("Fowaerts wurde gedrückt");
+            z.AddForce(y);
+        }
+
+    }
+}
