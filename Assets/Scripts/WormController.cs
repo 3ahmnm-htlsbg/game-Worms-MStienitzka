5819f88696a32b5642130b17e2e4592676f20ea2
max <max.stienitzka@gmail.com>
2020-06-09 15:01:26 +0200

    shoot function

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System.Security.Cryptography;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
+using Microsoft.Win32.SafeHandles;
 using UnityEngine;
 
 public class WormController : MonoBehaviour
@@ -7,24 +11,50 @@ public class WormController : MonoBehaviour
     // Start is called before the first frame update
     public KeyCode jumpKey;
     public KeyCode fowardKey;
-    public Rigidbody z;
+    public KeyCode backKey;
+    public KeyCode shootkey;
+    public Rigidbody rigid;
+    public Vector3 vecJump;
+    public Vector3 vecForward;
+    public Vector3 shootSpeed;
+    public GameObject projectile;
+    public GameObject spawnPosition;
 
-    public Vector3 x;
-    public Vector3 y;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(jumpKey))
         {
+            // void OnTriggerEnter(Collider other)
+            // {
+
+            // }
             Debug.Log("Jump wurde gedrückt");
-            z.AddForce(x);
+            rigid.AddForce(vecJump);
         }
 
-        if (Input.GetKeyDown(fowardKey))
+        if (Input.GetKey(fowardKey))
         {
+
             Debug.Log("Fowaerts wurde gedrückt");
-            z.AddForce(y);
+            rigid.AddForce(vecForward);
+        }
+
+        if (Input.GetKey(backKey))
+        {
+            Debug.Log("Back wurde gedrückt");
+            rigid.AddForce(-vecForward);
+        }
+
+        if (Input.GetKeyDown(shootkey))
+        {
+            Debug.Log("PewPew");
+
+            GameObject clone = Instantiate(projectile, spawnPosition.transform.position, Quaternion.identity);
+            Rigidbody rigid = clone.GetComponent<Rigidbody>();
+            rigid.AddForce(shootSpeed);
         }
 
     }
+
 }
